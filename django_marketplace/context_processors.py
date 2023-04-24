4919dd87cb55ec5919bbbf61f4eedb5be5618a67
Mikhailekb <appapa1012@gmail.com>
2023-04-24 13:56:42 +0500

    fix category

--- django_marketplace/context_processors.py
+++ django_marketplace/context_processors.py
@@ -4,7 +4,7 @@ from app_shops.models import Category
 from django_marketplace.constants import CATEGORIES_CACHE_LIFETIME
 
 
-def get_catalog(request):
+def get_categories(request):
     categories = cache.get_or_set('categories',
                                Category.objects.filter(is_active=True).select_related('parent').prefetch_related(
                                    'child_category'), timeout=CATEGORIES_CACHE_LIFETIME)
--- django_marketplace/settings.py
+++ django_marketplace/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django_marketplace.context_processors.get_catalog'
+                'django_marketplace.context_processors.get_categories'
             ],
         },
     },
