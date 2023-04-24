061a0d2c0c6e387fe6da6790bfc047ebca12b4ab
Ivanwg <81613195+Ivanwg@users.noreply.github.com>
2023-04-24 13:31:16 +0300

    settings file and extra css for header edited

--- django_marketplace/settings.py
+++ django_marketplace/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'phonenumber_field',
     'app_shops',
     'app_users',
     'phonenumber_field',
