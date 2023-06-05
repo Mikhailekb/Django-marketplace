70c5c5999d42da5d7387e6697b2203adccfe0416
Mikhailekb <appapa1012@gmail.com>
2023-06-05 19:03:27 +0500

    optimized code and added MoneyField

--- project_command/management/commands/start.py
+++ project_command/management/commands/start.py
@@ -1,5 +1,3 @@
-from sys import stdout
-from django.utils.translation import gettext_lazy as _
 from django.core import management
 from django.core.management.base import BaseCommand
 
@@ -11,6 +9,6 @@ class Command(BaseCommand):
         management.call_command('migration')
         management.call_command('fixtures')
         management.call_command('compilemessages', '--locale=ru', '--locale=en')
-        stdout.write(_('Getting the exchange rate...'))
+        print('Getting the exchange rate...')
         management.call_command('update_rates')
         management.call_command('createsuperuser')
