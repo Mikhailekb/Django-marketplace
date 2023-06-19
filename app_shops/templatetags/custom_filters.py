ea9e3eb6d7d0a4639e3c85cd51612b3a8fdf13a4
Mikhailekb <appapa1012@gmail.com>
2023-06-19 17:58:19 +0500

    added currency conversion in cart

--- app_shops/templatetags/custom_filters.py
+++ app_shops/templatetags/custom_filters.py
@@ -14,7 +14,7 @@ def dollar_conversion(value) -> Money | None:
     """Конвертация рублей в доллары"""
     if isinstance(value, Money):
         return convert_money(value, 'USD')
-    elif isinstance(value, (decimal.Decimal, float)):
+    elif isinstance(value, (decimal.Decimal, float, int)):
         return convert_money(Money(value, 'RUB'), 'USD')
     elif isinstance(value, str) and value.isdigit():
         return convert_money(Money(value, 'RUB'), 'USD')
