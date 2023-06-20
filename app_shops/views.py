699fbd5519095e694490e0fbe8ee4843b5b5d16d
Mikhailekb <appapa1012@gmail.com>
2023-06-20 13:50:58 +0500

    added reduction of goods in the shop when buying

--- app_shops/views.py
+++ app_shops/views.py
@@ -1,4 +1,4 @@
-import decimal
+from decimal import Decimal
 from collections import defaultdict
 from random import sample
 from typing import Any, Sequence
@@ -31,8 +31,6 @@ from .models.order import PaymentCategory, DeliveryCategory, Order, PaymentItem,
 from .models.product import SortProduct, Product, TagProduct, FeatureToProduct, Review
 from .models.shop import ProductShop
 
-
-Decimal = decimal.Decimal
 
 class HomeView(TemplateView):
     """
@@ -401,22 +399,20 @@ class OrderView(UserPassesTestMixin, FormView):
         city = form.cleaned_data.get('city')
         address = form.cleaned_data.get('address')
 
+        cart = Cart(self.request)
         order = Order.objects.create(buyer=self.request.user, delivery_category=delivery_category, name=name,
                                      phone=phone, email=email, city=city, address=address, comment=comment)
 
-        payment_category: PaymentCategory = form.cleaned_data.get('payment_category')
+        goods, error_messages = self._check_count_left_goods(cart, order)
 
-        goods = []
-        cart = Cart(self.request)
-        for product_shop_id, values in cart.cart.items():
-            price = values.get('price')
-            quantity = values.get('quantity')
-            item = OrderItem(order=order, product_shop_id=product_shop_id,
-                             price_on_add_moment=price, quantity=quantity)
-            goods.append(item)
+        if error_messages:
+            form.errors['not_enough_goods'] = error_messages
+            return super().form_invalid(form)
+
         OrderItem.objects.bulk_create(goods)
 
         total_price = cart.get_total_price()
+        payment_category: PaymentCategory = form.cleaned_data.get('payment_category')
         PaymentItem.objects.create(order=order, payment_category=payment_category, total_price=total_price)
 
         self.request.session['order'] = order.id
@@ -426,6 +422,25 @@ class OrderView(UserPassesTestMixin, FormView):
         elif payment_category.codename == 'some-other-way':
             self.success_url = reverse_lazy('home')
         return super().form_valid(form)
+
+    @staticmethod
+    def _check_count_left_goods(cart, order):
+        goods = []
+        error_messages = []
+        for product_shop_id, values in cart.cart.items():
+            if product_shop := ProductShop.objects.filter(id=product_shop_id, is_active=True).get():
+                price = values.get('price')
+                quantity = values.get('quantity')
+                if product_shop.count_left - quantity < 0:
+                    not_valid = True
+                    name = product_shop.product.name
+                    message = _(f"{name}: in stock - {product_shop.count_left}, in cart - {quantity}")
+                    error_messages.append(message)
+                else:
+                    item = OrderItem(order=order, product_shop_id=product_shop_id,
+                                     price_on_add_moment=price, quantity=quantity)
+                    goods.append(item)
+        return goods, error_messages
 
 
 class PaymentView(UserPassesTestMixin, TemplateView):
@@ -456,14 +471,25 @@ class PaymentView(UserPassesTestMixin, TemplateView):
         order_id = self.request.session.get('order', None)
         payment = PaymentItem.objects.get(order_id=order_id)
         payment.from_account = account
-        order = Order.objects.get(id=order_id)
+        order: Order = Order.objects.get(id=order_id)
 
         if last_sym.isdigit() and int(last_sym) % 2 == 0:
             payment.is_passed = True
             order.is_paid = True
+
+            products_to_update = []
+            for order_item in order.items.all():
+                quantity = order_item.quantity
+                product_shop = order_item.product_shop
+
+                product_shop.count_left -= quantity
+                product_shop.count_sold += quantity
+
+                products_to_update.append(product_shop)
+            ProductShop.objects.bulk_update(products_to_update, ['count_left', 'count_sold'])
+
         payment.save()
         order.save()
-
         return redirect(reverse('payment_progress'))
 
 
