1c13863288f751bfbc1a77246b371c0c21669e37
kirill.kuverin@gmail.com <kirill.kuverin@gmail.com>
2023-06-30 15:09:20 +0300

    fix product avg price

--- app_shops/views.py
+++ app_shops/views.py
@@ -26,6 +26,7 @@ from .models.banner import Banner, SpecialOffer, SmallBanner
 from .models.discount import Discount
 from .models.product import SortProduct, Product, TagProduct, FeatureToProduct, Review
 from .models.shop import ProductShop
+from .services.functions import get_prices, price_exp, price_exp_banners
 
 
 class HomeView(TemplateView):
@@ -37,20 +38,20 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         goods = Product.objects.select_related('category', 'main_image') \
-            .prefetch_related(Prefetch('in_shops', queryset=ProductShop.objects.select_related('shop')))
-        top_products = goods.order_by(
-            '-in_shops__count_sold')[:8].annotate(avg_price=Avg('in_shops__price'))
+            .prefetch_related(Prefetch('in_shops', queryset=ProductShop.objects.select_related('shop'))) \
+            .annotate(Sum('in_shops__count_sold')).order_by('-in_shops__count_sold__sum')[:8]\
+            .annotate(avg_price=Avg(price_exp))
 
         banners = Banner.objects.filter(is_active=True)[:3].select_related('product')
 
         small_banners = SmallBanner.objects.all()[:3].select_related('product').annotate(
-            price_from=Min('product__in_shops__price'))
+            price_from=Min(price_exp_banners))
         if product_with_timer := SpecialOffer.objects.all().first():
             context['product_with_timer'] = ProductShop.objects.with_discount_price() \
                 .get(id=product_with_timer.product_shop_id)
             context['date_end'] = product_with_timer.date_end.strftime('%d.%m.%Y %H:%M')
 
-        context['top_goods'] = top_products
+        context['top_goods'] = goods
         context['banners'] = banners
         context['small_banners'] = small_banners
 
@@ -77,13 +78,15 @@ class CatalogView(FilterView):
         filter_options = {'is_active': True, 'in_shops__is_active': True}
         if category := self.request.GET.get('category'):
             filter_options['category__slug'] = category
+
         self.queryset = Product.objects.filter(**filter_options) \
             .select_related('category', 'main_image') \
-            .annotate(avg_price=Avg('in_shops__price'),
-                      min_price=Min('in_shops__price'),
-                      max_price=Max('in_shops__price'),
+            .annotate(avg_price=Avg(price_exp),
+                      min_price=Min(price_exp),
+                      max_price=Max(price_exp),
                       count_sold=Sum('in_shops__count_sold'),
                       feedback=Count('reviews')).order_by('count_sold')
+
         return self.queryset
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -120,6 +123,7 @@ class CatalogView(FilterView):
         else:
             price_from = convert_money(Money(min_price, 'RUB'), 'USD').amount
             price_to = convert_money(Money(max_price, 'RUB'), 'USD').amount
+
         return min_price, max_price, price_from, price_to
 
 
@@ -218,7 +222,7 @@ class ProductDetailView(DetailView):
                              Prefetch('features', queryset=FeatureToProduct.objects.select_related('feature_name')
                                       .prefetch_related('values')),
                              Prefetch('in_shops', queryset=ProductShop.objects.select_related('shop')),
-                             Prefetch('reviews', queryset=Review.objects.select_related('profile')))
+                             Prefetch('reviews', queryset=Review.objects.select_related('profile'))).annotate(avg_price=Avg(price_exp))
 
         return queryset
 
@@ -227,11 +231,10 @@ class ProductDetailView(DetailView):
         product: Product = context['product']
         discounts_query = ProductShop.objects.with_discount_price().select_related('shop').filter(product=product)
         reviews_count = product.reviews.count()
-        shop_prices, price = self.get_prices(discounts_query)
+        shop_prices = get_prices(discounts_query)
         cart_product_form = CartAddProductForm()
 
         context['review_form'] = ReviewForm
-        context['price'] = price
         context['sellers'] = shop_prices
         context['reviews_count'] = reviews_count
         context['cart_product_form'] = cart_product_form
@@ -251,20 +254,6 @@ class ProductDetailView(DetailView):
             review.save()
         return redirect('product-detail', product_slug=product.slug)
 
-    @staticmethod
-    def get_prices(discounts_query):
-        shop_prices = {product_shop: {'price_old': product_shop.price.amount}
-        if not product_shop.discount_price
-        else {'price_old': product_shop.price.amount, 'price_new': product_shop.discount_price}
-                       for product_shop in discounts_query}
-        price_list = [price.get('price_new') or price.get('price_old')
-                      for price in shop_prices.values()]
-
-        price = float(sum([float(price) if not isinstance(price, Money) else float(price.amount)
-                           for price in price_list]) / len(price_list))
-
-        return shop_prices, price
-
 
 class ComparisonView(TemplateView):
     MAX_VALUE = 3
@@ -276,7 +265,7 @@ class ComparisonView(TemplateView):
             'comparison_products', default=[])[:3]
         if comparison_products and isinstance(comparison_products, list) and len(comparison_products) <= self.MAX_VALUE:
             goods: QuerySet[Product] = Product.objects.filter(id__in=comparison_products) \
-                .annotate(avg_price=Avg('in_shops__price')) \
+                .annotate(avg_price=Avg(price_exp)) \
                 .select_related('category', 'main_image')
 
             if len({item.category_id for item in goods}) == 1:
